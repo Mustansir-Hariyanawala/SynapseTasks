65cc52856b62054686d50b3883da584f8d995aeb
MURTUZA HARIYANAWALA <mustansirhy@gmail.com>
2024-08-10 10:42:38 +0530

    Completion of Task 3

---
+++ Tech/task-1/task_1.js
@@ -0,0 +1,8 @@
+let start = document.querySelector(".input1").textContent;
+let end = document.querySelector(".input2").textContent;
+
+let obj = {};
+
+for (let i = start; i < end; i++) {
+    if()
+}
